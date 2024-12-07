f9cd0d098b4df7b0e4b0622ac23e446d1681b604
EC2 Default User <ec2-user@ip-172-31-91-11.ec2.internal>
2024-12-07 09:33:31 +0000

    Initial commit

---
+++ test/proj4.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Proj4 from '../lib/proj4-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/proj4-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Proj4.Proj4Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
